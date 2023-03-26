0dd6f56deba53d7c8f1a9e387f5b766abfe8399e
Abdalla <qoryoley@gmail.com>
2023-03-26 23:01:50 +0300

    Completed Destructing Assigments

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const farmAnimals = 'cow horse sheep pig chicken';
-
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [bessie, , dolly, babe, little] = farmAnimals.split(" ");
+const [blackAndWhite, , black, pink,] = farmAnimals.split(" ");
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r, o, y, g, b, , v] = colors;
+const [, , , , , indg , ] = colors;
+
+
+
+
 
 const muppet = {
+  
   muppetName: 'Miss Piggy',
   color: 'pink',
   song: 'Never Before, Never Again',
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
-
+const {muppetName, color, song, job, partner} = muppet
 const nestedMuppet = {
   nestedName: 'Kermit',
   nestedColor: 'green',
@@ -21,9 +31,14 @@ const nestedMuppet = {
       song4: 'I Hope That Something Better Comes Along',
     },
   },
+  
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
+  
 };
+const {song2, song4} = nestedMuppet.album.theMuppetMovie
+const nestedJob= nestedMuppet.nestedJob
+const nestedPartner = nestedMuppet.nestedPartner
 
 // Strings
 
